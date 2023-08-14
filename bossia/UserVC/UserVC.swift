48eabae302186d0d946eafee77be2631cdc1b6cd
Onur Bostan <onurbosstan@gmail.com>
2023-08-14 23:42:02 +0300

    added UITextFieldDelegate && added textFieldShouldReturn

--- bossia/UserVC/UserVC.swift
+++ bossia/UserVC/UserVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class UserVC: UIViewController {
+class UserVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
@@ -18,28 +18,33 @@ class UserVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        emailText.delegate = self
+        passwordText.delegate = self
+        
         let fireStoreDB = Firestore.firestore()
         fireStoreDB.collection("User").whereField("mail", isEqualTo: emailText.text!).getDocuments { (snapshot, error) in
-            if error != nil {
+            if error != nil
+            {
                 self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
-            } else {
-                for document in snapshot!.documents {
+            } else
+            {
+                for document in snapshot!.documents
+                {
                     let mail = document.get("mail")
                     print(mail!)
                     self.performSegue(withIdentifier: "toHomeVC", sender: nil)
                 }
             }
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailText.resignFirstResponder()
+        passwordText.resignFirstResponder()
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
         
-        /*
-        //Auth = YETKİ
-        //Current User(Aktif kullanıcı) = Kayıtlı kullanıcının otomatik girebilmesi için;
-        let currentUser = Auth.auth().currentUser
-        if currentUser != nil
-        {
-            self.performSegue(withIdentifier: "toHomeVC", sender: nil)
-        }
-         */
     }
     
     
@@ -68,17 +73,19 @@ class UserVC: UIViewController {
         if emailText.text != "" && passwordText.text != ""
         {
             Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
-                if error != nil {
-                    
+                if error != nil
+                {
                     self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
                 } else
                 {
                     let fireStoreDB = Firestore.firestore()
                     let fireStoreUser = ["mail" : Auth.auth().currentUser!.email!, "password": self.passwordText.text!] as? [String : Any]
                     fireStoreDB.collection("User").addDocument(data: fireStoreUser!) { (error) in
-                        if error != nil {
+                        if error != nil
+                        {
                             self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
-                        } else {
+                        } else
+                        {
                             self.makeAlert(titleInput: "Success!", messageInput: "Success!")
                         }
                     }
