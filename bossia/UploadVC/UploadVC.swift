27d004bdec1bc4cfd1a09ccf6e34b8d9cb88ef02
Onur Bostan <onurbosstan@gmail.com>
2023-09-02 20:48:08 +0300

    camera usage has been activated.

--- bossia/UploadVC/UploadVC.swift
+++ bossia/UploadVC/UploadVC.swift
@@ -14,12 +14,13 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     @IBOutlet weak var commentText: UITextField!
     
     let imagePicker = UIImagePickerController()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGestureRecognizer)
         
         imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
@@ -36,45 +37,53 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        imageView.image = info[.originalImage] as? UIImage
-        self.dismiss(animated: true)
+        if let selectedImage = info[.originalImage] as? UIImage {
+                imageView.image = selectedImage
+            }
+            dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
     }
     
     @IBAction func actionButtonClicked(_ sender: Any)
     {
-        let storage = Storage.storage()
-        let storageReference = storage.reference() //referans belirleriz; gideceğimiz dosyayı söyleriz;
-        let mediaFolder = storageReference.child("media") //   "media" isminde bir klasör açtık;
-        if let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        if let imageData = imageView.image?.jpegData(compressionQuality: 0.5)
         {
             let uuid = UUID().uuidString
-            let imageReference = mediaFolder.child("\(uuid).jpg")
-            imageReference.putData(data) { (storageMetadata, error) in
-                if error != nil
+            let imageReference = Storage.storage().reference().child("media").child("\(uuid).jpg")
+
+            imageReference.putData(imageData, metadata: nil) { (storageMetadata, error) in
+                if let error = error
                 {
-                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!, Try again!")
+                    self.makeAlert(titleInput: "Error!", messageInput: error.localizedDescription)
                 } else
                 {
                     imageReference.downloadURL { (url, error) in
-                        if error == nil {
-                            let imageUrl = url?.absoluteString
-                            if let imageUrl = imageUrl
-                            {
-                                
-                                //DATABASE
-                                
-                                let fireStoreDatabase = Firestore.firestore()
-                                let firestorePost = ["imageurl" : imageUrl, "comment" : self.commentText.text!, "email" : Auth.auth().currentUser!.email!, "date" : FieldValue.serverTimestamp(), "like" : 0] as [String : Any]
-                                fireStoreDatabase.collection("Post").addDocument(data: firestorePost) { (error) in
-                                    if error != nil {
-                                        self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
-                                    } else
-                                    {
-                                        self.commentText.text = ""
-                                        self.imageView.image = UIImage(named: "select")
-                                        self.tabBarController?.selectedIndex = 0
-                                        
-                                    }
+                        if let error = error
+                        {
+                            self.makeAlert(titleInput: "Error!", messageInput: error.localizedDescription)
+                        } else if let imageUrl = url?.absoluteString
+                        {
+                            let firestoreDatabase = Firestore.firestore()
+                            let postDocument = firestoreDatabase.collection("Members").document(Auth.auth().currentUser!.email!).collection("Posts").document()
+                            let postDictionary: [String: Any] = [
+                                "imageurl": imageUrl,
+                                "comment": self.commentText.text ?? "",
+                                "email": Auth.auth().currentUser!.email ?? "",
+                                "date": FieldValue.serverTimestamp(),
+                                "like": 0
+                            ]
+                            postDocument.setData(postDictionary) { error in
+                                if let error = error
+                                {
+                                    self.makeAlert(titleInput: "Error!", messageInput: error.localizedDescription)
+                                } else
+                                {
+                                    self.commentText.text = ""
+                                    self.imageView.image = UIImage(named: "select")
+                                    self.tabBarController?.selectedIndex = 0
                                 }
                             }
                         }
@@ -84,15 +93,25 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
         }
     }
     
-    
-    
-    
-   
+    @IBAction func openCameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera)
+        {
+                    imagePicker.sourceType = .camera
+                    imagePicker.allowsEditing = false
+                    imagePicker.delegate = self
+                    present(imagePicker, animated: true, completion: nil)
+                    
+                } else
+                {
+                    print("Kamera kullanılamıyor.")
+                }
+    }
+
     func makeAlert(titleInput: String, messageInput: String)
     {
         let alert = UIAlertController(title: title, message: messageInput, preferredStyle: .alert)
         let okButton = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okButton)
         self.present(alert, animated: true)
-    }
+    }  
 }
